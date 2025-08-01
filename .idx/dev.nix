a9c3075062e932631ef53f6edb2d57298230cd37
miguelmoyaortega <miguelmoyaortega@gmail.com>
2025-08-01 19:03:22 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,15 +2,11 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable"; # or "stable-24.05"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_22
   ];
 
   # Sets environment variables in the workspace
@@ -25,16 +21,23 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = [
+            "npm"
+            "start"
+            "--"
+            "--host"
+            "0.0.0.0"
+            "--port"
+            "$PORT"
+          ];
+          manager = "web";
+          cwd = "app";
+        };
+        emulators = {
+          command = ["firebase" "emulators:start"];
+          manager = "web";
+        };
       };
     };
 
@@ -42,8 +45,9 @@
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        npm-install = "npm install";
+        npm-install-app = "cd app && npm install";
+        npm-install-functions = "cd functions && npm install";
       };
       # Runs when the workspace is (re)started
       onStart = {
