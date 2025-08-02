700c55b067e3503425950ccd4b85e5043e8e9812
miguelmoyaortega <miguelmoyaortega@gmail.com>
2025-08-02 19:19:36 +0000

    create error component

--- app/src/app/features/pets/components/pet-card/pet-card.ts
+++ app/src/app/features/pets/components/pet-card/pet-card.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { PetEntity } from '../../entities/pet.entity';
 
 @Component({
   selector: 'app-pet-card',
   imports: [],
-  template: ``
+  template: `
+        @let petTemplate = pet();
+        <img class="w-full" src="{{ petTemplate.imagesUrl[0] }}" alt="{{ petTemplate.name }}">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{{ petTemplate.name }}</div>
+          <p class="text-gray-700 text-base">
+            {{ petTemplate.description }}
+          </p>
+        </div>
+  `,
+  host: {
+    class: "max-w-sm rounded overflow-hidden shadow-lg"
+  }
 })
 export class PetCard {
-
+  public readonly pet = input.required<PetEntity>();
 }
